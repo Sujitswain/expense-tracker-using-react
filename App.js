361111df026d975b2cbbcfbb7087b2b605fb0c47
SUJIT SWAIN <90376564+Sujitswain@users.noreply.github.com>
2023-10-03 09:42:44 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Expense_Tracker from './Expense_Tracker/controller'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Expense_Tracker />
+    </div>
+  );
+}
+
+export default App;
