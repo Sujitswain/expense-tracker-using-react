4a7e34fee7531fbd4e037633842cc2eb0da03f8c
SUJIT SWAIN <90376564+Sujitswain@users.noreply.github.com>
2023-10-03 09:08:42 +0530

    Create IncomeExpenses.js

---
+++ components/IncomeExpenses.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+//Money formatter function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split('.');
+  return (
+    '$ ' +
+    p[0]
+      .split('')
+      .reverse()
+      .reduce(function (acc, num, i) {
+        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+      }, '') +
+    '.' +
+    p[1]
+  );
+}
+
+export const IncomeExpenses = () => {
+
+    // using the Context
+    const { transactions } = useContext(GlobalContext);
+
+    // maps the amounts and store in amounts array
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    // count income if amount is positive    
+    const income = amounts
+      .filter(item => item > 0)
+      .reduce((acc, item) => (acc += item), 0);
+
+    // count expense if amount is negative
+    const expense = (
+      amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+      -1
+    );
+
+    return (
+        <div className="inc-exp-container">
+
+          <div>
+            <h4>Income</h4>
+            <p className="money plus">{moneyFormatter(income)}</p>
+          </div>
+          
+          <div>
+            <h4>Expense</h4>
+            <p className="money minus">{moneyFormatter(expense)}</p>
+          </div>
+        
+        </div>
+    )
+}
