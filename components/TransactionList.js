d5a6d1e0ce7bf8b4d52b6b7c9ba5dbd02fe3ad7f
SUJIT SWAIN <90376564+Sujitswain@users.noreply.github.com>
2023-10-03 09:12:45 +0530

    Create TransactionList.js

---
+++ components/TransactionList.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
+
+export const TransactionList = () => {
+
+  // use the transactions array of objects using Context
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3>History</h3>
+
+      <ul className="list">
+        {
+          transactions.map(transaction => (
+          <Transaction 
+            key={transaction.id} 
+            transaction={transaction} 
+          />
+          ))
+        }
+      </ul>
+    </>
+  )
+}
